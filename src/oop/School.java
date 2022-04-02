b45c11c2aa5d539ac89e6f7d4efa654192e41686
Thiti.Prove <32426927+Disthanitian@users.noreply.github.com>
2022-04-02 13:23:36 +0700

    [utility]: onMethod

--- src/oop/School.java
+++ src/oop/School.java
@@ -2,13 +2,24 @@ package oop;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-@Data
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 @SuperBuilder()
+@Valid
 public class School extends Building {
 
+    @Getter
+    @Setter(onMethod_ = {@Max(10), @NotNull})
     private String name;
+
+    @Getter
+    @Setter
     private String address;
 
 }
--- src/services/SchoolService.java
+++ src/services/SchoolService.java
@@ -2,6 +2,11 @@ package services;
 
 import oop.School;
 
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 public class SchoolService {
 
     public static void main(String[] args){
