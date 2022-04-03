4cde4a083e4bfd424ea13a9b14a424b728ced548
Thiti.Prove <32426927+Disthanitian@users.noreply.github.com>
2022-04-03 14:10:32 +0700

    [log]: @Log

---
+++ src/services/LogService.java
@@ -0,0 +1,19 @@
+package services;
+
+import lombok.extern.java.Log;
+
+import java.util.logging.Logger;
+
+@Log
+public class LogService {
+
+
+   public static void main(String[] args){
+
+       log.info("This is my first log");
+       log.info("This is second log");
+
+   }
+
+
+}
